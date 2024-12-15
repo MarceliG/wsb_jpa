fc5bf6672633179617972b818889fe679dfaffc0
MarceliG <marcel9817@gmail.com>
2024-12-15 20:37:14 +0100

    zad 2 and 3

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/PatientDao.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/PatientDao.java
@@ -1,7 +1,10 @@
 package com.jpacourse.persistence.dao;
 
+import java.time.LocalDateTime;
+
 import com.jpacourse.persistence.entity.PatientEntity;
 
 public interface PatientDao extends Dao<PatientEntity, Long> {
+    void addVisitToPatient(Long patientId, Long doctorId, LocalDateTime visitTime, String visitDescription);
 
 }
